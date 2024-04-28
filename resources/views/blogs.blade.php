83194e76630f8776de51d8fd46f5513531c7a2c7
Moe Myint Zaw <moemyintzaw17@gmail.com>
2024-04-28 13:41:33 +0630

    draw-back-of-static-or-hardcoding-website

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <h1>First Blog</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint eius consequatur tempora placeat quaerat numquam, labore 
+        aliquam blanditiis dignissimos ipsam esse, voluptate non quo illo reprehenderit iure natus, amet adipisci!</p>
+
+    <h1>Second Blog</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint eius consequatur tempora placeat quaerat numquam, labore 
+    aliquam blanditiis dignissimos ipsam esse, voluptate non quo illo reprehenderit iure natus, amet adipisci!</p>
+
+    <h1>Third Blog</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint eius consequatur tempora placeat quaerat numquam, labore 
+    aliquam blanditiis dignissimos ipsam esse, voluptate non quo illo reprehenderit iure natus, amet adipisci!</p>
+</body>
+</html>
